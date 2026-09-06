9b22fdf39ac604c58e770f26f62beb840c309111
lymyr <176290600+lymyr@users.noreply.github.com>
2026-09-06 12:36:47 +0800

    feat: added file validations

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,15 +1,22 @@
 import { prisma } from "../lib/prisma.js"
 import path from "node:path"
 import { deleteFileTask } from "../lib/helpers.js"
+import { fileVadiation } from "../lib/validations.js"
+import { validationResult } from "express-validator"
 
 export const uploadFile = async (req, res) => {
+    if (req.files.length == 0)
+        throw new Error("Please upload a file")
+
     const fileList = req.files.map(file => {
+        if (file.originalname.length > 200)
+            throw new Error("File name must not exceed 200 characters")
         return {
             path: file.destination,
             id: file.filename,
             name: file.originalname,
             bytes: parseInt(file.size),
-            folderId: parseInt(req.body.currentFolder)
+            folderId: parseInt(req.body.parentId)
         }
     })
 
@@ -17,7 +24,7 @@ export const uploadFile = async (req, res) => {
         data: fileList
     })
 
-    res.redirect(`/folder/${req.body.currentFolder}`)
+    res.redirect(`/folder/${req.body.parentId}`)
 }
 
 export const deleteFile = async (req, res) => {
@@ -33,13 +40,18 @@ export const redirectUpdate = async (req, res) => {
     res.redirect(`/file/${req.body.redirectId}/${req.body.fileId}`)
 }
 
-export const viewUpdate = async (req, res) => {
+export const viewUpdate = async (req, res, next) => {
+    if (req.errors && !req.errors.update)
+        return next()
+    const fileId = req.body?.id ? req.body.id : req.params.updateId
+    const folderId = req.body?.parentId ? req.body.parentId : req.params.folderId
+
     const q = []
     q.push(prisma.file.findFirstOrThrow({
-        where: {id: req.params.updateId}
+        where: {id: fileId}
     }))
     q.push(prisma.folder.findFirstOrThrow({
-        where: {id: parseInt(req.params.folderId)},
+        where: {id: parseInt(folderId)},
         include: {child: true, file: true}
     }))
     const [updateFile, folder] = await Promise.all(q)
@@ -47,18 +59,43 @@ export const viewUpdate = async (req, res) => {
     res.render('folderPage', {
         user: req.user, 
         currentFolder: folder, 
-        updateFile: updateFile
+        updateFile: updateFile,
+        errors: req.errors
     })
 }
 
-export const updateFile = async (req, res) => {
-    console.log("in")
-    await prisma.file.update({
-        where: {id: req.body.id},
-        data: {
-            name: req.body.fileName,
-            folderId: parseInt(req.body.parentId)
+export const updateFile = [
+    fileVadiation.name,
+    async (req, res) => {
+        const err = validationResult(req)
+        if (!err.isEmpty()) {
+            const e = new Error(err.mapped().fileName.msg)
+            e.update = true
+            throw e
         }
-    })
-    res.redirect(`/folder/${req.body.parentId}`)
+            
+        await prisma.file.update({
+            where: {id: req.body.id},
+            data: {
+                name: req.body.fileName,
+                folderId: parseInt(req.body.parentId)
+            }
+        })
+        res.redirect(`/folder/${req.body.parentId}`)
+    }
+]
+
+export const fileError = (err, req, res, next) => {
+    if (err.code == 'LIMIT_FILE_SIZE')
+        err.message = 'File should not exceed 5mb'
+    else if (err.code == 'LIMIT_UNEXPECTED_FILE')
+        err.message = 'Please limit the number of files to 3'
+
+    req.errors = {
+        files: {
+            msg: err.message
+        },
+        update: err.update
+    }
+    next()
 }
--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
-import { deleteFile, downloadFile, redirectUpdate, updateFile, uploadFile, viewUpdate } from "../controllers/fileController.js";
+import { deleteFile, downloadFile, fileError, redirectUpdate, updateFile, uploadFile, viewUpdate } from "../controllers/fileController.js";
 import multer from "multer";
+import { viewFolder } from "../controllers/folderController.js";
 
 const fileRouter = Router()
 
-const uploadMiddleware = multer({ dest: 'uploads/' })
+const uploadMiddleware = multer({ dest: 'uploads/', limits: { fileSize: 5000000 } })
 
-fileRouter.post('/upload', uploadMiddleware.array('files', 15), uploadFile)
+fileRouter.post('/upload', uploadMiddleware.array('files', 3), uploadFile, viewFolder)
 fileRouter.post('/delete', deleteFile)
 fileRouter.post('/download', downloadFile)
 fileRouter.post('/redirect/update', redirectUpdate)
 fileRouter.get('/:folderId/:updateId', viewUpdate)
 fileRouter.post('/update', updateFile)
+
+fileRouter.use(fileError, viewUpdate, viewFolder)
+
 
 export default fileRouter
